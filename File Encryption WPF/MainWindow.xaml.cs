90a8fb2c0da1439f055e97973cdefd7c66466910
AliShamil <elisamilzade@gmail.com>
2023-02-22 19:57:51 +0400

    Last Update

--- File Encryption WPF/MainWindow.xaml.cs
+++ File Encryption WPF/MainWindow.xaml.cs
@@ -23,212 +23,243 @@ namespace File_Encryption_WPF;
 
 public partial class MainWindow : Window
 {
-    private CancellationTokenSource _cts;
+    string ivAsBase64;
+    string keyAsBase64;
+    string encryptedTextAsBase64;
+    int _keySize;
+    private CancellationTokenSource? _cts;
+    public string FilePath { get; set; } = null!;
 
     public MainWindow()
     {
         InitializeComponent();
+        DataContext = this;
+        ivAsBase64 = string.Empty;
+        keyAsBase64 = string.Empty;
+        encryptedTextAsBase64 = string.Empty;
+        _keySize = 128;
+    }
+    private void Window_Loaded(object sender, RoutedEventArgs e)
+       => rbtn128.IsChecked= true;
+
+
+    private void btnFile_Click(object sender, RoutedEventArgs e)
+    {
+        FileDialog dialog = new OpenFileDialog();
+        dialog.Filter = "Text files (*.txt)|*.txt";
+
+        var result = dialog.ShowDialog();
+
+        if (result == true)
+            txtPath.Text = dialog.FileName;
     }
 
-    private void BrowseButton_Click(object sender, RoutedEventArgs e)
+    private void btnStart_Click(object sender, RoutedEventArgs e)
     {
-        var openFileDialog = new OpenFileDialog();
-        if (openFileDialog.ShowDialog() == true)
-        {
-            txtPath.Text = openFileDialog.FileName;
-        }
-    }
+        var chararcterSize = _keySize/8;
 
-    private void StartButton_Click(object sender, RoutedEventArgs e)
-    {
-        if (string.IsNullOrEmpty(txtPath.Text))
+        StringBuilder sb = new();
+
+        if (string.IsNullOrWhiteSpace(FilePath))
         {
-            MessageBox.Show("Please choose a file.");
+            MessageBox.Show("Enter File Path");
             return;
         }
 
-        if (string.IsNullOrEmpty(txtPassword.Password))
+        if (!File.Exists(FilePath))
+            sb.Append($"'{FilePath}' was not found\n");
+
+        if (rbtnDecrypt.IsChecked == false && rbtnEncrypt.IsChecked == false)
+            sb.Append("Action(encrypt/decrypt) must be choosen!\n");
+
+        if (string.IsNullOrWhiteSpace(txtPassword.Password))
+            sb.Append("Password must be written for encryption!");
+
+        if (txtPassword.Password.Length!=chararcterSize)
+            sb.Append($"Password must contain {chararcterSize} characters");
+
+        if (sb.Length > 0)
         {
-            MessageBox.Show("Please enter a password.");
+            MessageBox.Show(sb.ToString());
             return;
         }
 
-        if (!File.Exists(txtPath.Text))
-        {
-            MessageBox.Show("The selected file does not exist.");
-            return;
-        }
-
-        bool isEncryptMode = rbtnEncrypt.IsChecked == true;
-        if (isEncryptMode && File.Exists(txtPath.Text + ".encrypted"))
-        {
-            MessageBox.Show("The file has already been encrypted.");
-            return;
-        }
-
-        if (!isEncryptMode && !File.Exists(txtPath.Text + ".encrypted"))
-        {
-            MessageBox.Show("The file has not been encrypted.");
-            return;
-        }
-
-        btnStart.IsEnabled = false;
-        btnSelectPath.IsEnabled = false;
-        btnCancel.IsEnabled = true;
+        progressBar.Value = 0;
 
         _cts = new CancellationTokenSource();
-        var cancellationToken = _cts.Token;
 
-        ThreadPool.QueueUserWorkItem(state =>
+        if (rbtnEncrypt.IsChecked == true)
+            EncryptAndWriteToFile(_cts.Token);
+        else
+            DecryptAndWriteToFile(_cts.Token);
+
+    }
+
+    private void txtPassword_LostFocus(object sender, RoutedEventArgs e)
+    {
+        var chararcterSize = _keySize/8;
+        if (txtPassword.Password.Length <chararcterSize)
         {
-            try
+            MessageBox.Show($"You need to write at least {chararcterSize} characters");
+        }
+
+    }
+
+    private void rbtnKeySize_Checked(object sender, RoutedEventArgs e)
+    {
+        if (!string.IsNullOrEmpty(txtPassword.Password))
+            txtPassword.Password = string.Empty;
+
+        var name = (sender as RadioButton).Name;
+
+        switch (string.Join("", name.ToCharArray().Where(Char.IsDigit)))
+        {
+            case "128":
+                _keySize= 128;
+                break;
+            case "192":
+                _keySize= 192;
+                break;
+            case "256":
+                _keySize= 256;
+                break;
+        }
+        txtPassword.MaxLength= _keySize/8;
+    }
+
+
+    private void EncryptAndWriteToFile(CancellationToken token)
+    {
+        var text = File.ReadAllText(FilePath);
+
+        var password = txtPassword.Password;
+
+        var bytesToWrite = Encryp(ref ivAsBase64, ref keyAsBase64, ref encryptedTextAsBase64, text, password);
+
+        btnStart.IsEnabled = false;
+        btnCancel.IsEnabled = true;
+
+        ThreadPool.QueueUserWorkItem(o =>
+        {
+            using var fs = new FileStream(FilePath, FileMode.Truncate);
+
+            for (int i = 0; i < bytesToWrite.Length; i++)
             {
 
-                if (isEncryptMode)
+                if (token.IsCancellationRequested)
                 {
-
-                    this.Dispatcher.Invoke(() =>
-                    {
-                    EncryptFile(txtPath.Text, txtPath.Text + ".encrypted", txtPassword.Password, cancellationToken);
-                        
-                    });
+                    fs.Dispose();
+                    Dispatcher.Invoke(() => File.WriteAllText(FilePath, text));
+                    Dispatcher.Invoke(() => progressBar.Value = 0);
+                    Dispatcher.Invoke(() => btnStart.IsEnabled = true);
+                    return;
                 }
-                else
-                {
 
-                    this.Dispatcher.Invoke(() =>
-                    {
-                    DecryptFile(txtPath.Text + ".encrypted", txtPath.Text, txtPassword.Password, cancellationToken);
-                        
-                    });
-                }
 
-                this.Dispatcher.Invoke(() =>
-                {
-                    MessageBox.Show("Done!");
-                });
+                if (i != 0)
+                    Dispatcher.Invoke(() => progressBar.Value = 100 * i / bytesToWrite.Length);
+
+                fs.WriteByte(bytesToWrite[i]);
+                Thread.Sleep(1);
             }
-            catch (OperationCanceledException)
-            {
-                this.Dispatcher.Invoke(() =>
-                {
-                    MessageBox.Show("Canceled.");
-                });
-            }
-            catch (Exception ex)
-            {
-                this.Dispatcher.Invoke(() =>
-                {
-                    MessageBox.Show("Error: " + ex.Message);
-                });
-            }
-            finally
-            {
-                this.Dispatcher.Invoke(() =>
-                {
-                    btnStart.IsEnabled = true;
-                    btnSelectPath.IsEnabled = true;
-                    btnCancel.IsEnabled = false;
-                    progressBar.Value = 0;
-                });
-            }
+
+            fs.Seek(0, SeekOrigin.Begin);
+
+            Dispatcher.Invoke(() => btnStart.IsEnabled = true);
+            Dispatcher.Invoke(() => btnCancel.IsEnabled = false);
+            Dispatcher.Invoke(() => progressBar.Value = 100);
         });
     }
 
-    private void EncryptFile(string inputFilePath, string outputFilePath, string password, CancellationToken cancellationToken)
+    private void DecryptAndWriteToFile(CancellationToken token)
     {
-        using var inputStream = new FileStream(inputFilePath, FileMode.Open, FileAccess.Read);
-        using var outputStream = new FileStream(outputFilePath, FileMode.Create, FileAccess.Write);
+        var bytes = File.ReadAllBytes(FilePath);
 
-        var aes = Aes.Create();
-        aes.Key = GetKey(password, aes);
-        aes.IV = GetRandomBytes(16);
+        var password = txtPassword.Password;
 
-        var encryptor = aes.CreateEncryptor();
+        var bytesToWrite = Decryp(ref ivAsBase64, ref keyAsBase64, ref encryptedTextAsBase64);
+        var text = Encoding.UTF8.GetString(bytesToWrite); ;
 
-        using var cryptoStream = new CryptoStream(outputStream, encryptor, CryptoStreamMode.Write);
+        btnStart.IsEnabled = false;
+        btnCancel.IsEnabled = true;
 
-        var buffer = new byte[4096];
-        long totalBytes = inputStream.Length;
-        long processedBytes = 0;
-
-        while (true)
+        ThreadPool.QueueUserWorkItem(o =>
         {
-            cancellationToken.ThrowIfCancellationRequested();
+            using var fs = new FileStream(FilePath, FileMode.Truncate);
 
-            int bytesRead = inputStream.Read(buffer, 0, buffer.Length);
-            Thread.Sleep(1500);
-            if (bytesRead == 0) break;
+            for (int i = 0; i < bytesToWrite.Length; i++)
+            {
 
-            cryptoStream.Write(buffer, 0, bytesRead);
-            processedBytes += bytesRead;
-            int progress = (int)Math.Round(processedBytes * 100.0 / totalBytes);
-            progressBar.Value = progress;
-        }
-        inputStream.Seek(0, SeekOrigin.Begin);
-        outputStream.Seek(0, SeekOrigin.Begin);
-        outputStream.Flush();
+                if (token.IsCancellationRequested)
+                {
+                    fs.Dispose();
+                    Dispatcher.Invoke(() => File.WriteAllBytes(FilePath, bytes));
+                    Dispatcher.Invoke(() => progressBar.Value = 0);
+                    Dispatcher.Invoke(() => btnStart.IsEnabled = true);
+                    return;
+                }
+
+                if (i != 0)
+                    Dispatcher.Invoke(() => progressBar.Value = 100 * i / bytesToWrite.Length);
+
+                fs.WriteByte(bytesToWrite[i]);
+                Thread.Sleep(1);
+            }
+
+            fs.Seek(0, SeekOrigin.Begin);
+
+            Dispatcher.Invoke(() => btnStart.IsEnabled = true);
+            Dispatcher.Invoke(() => btnCancel.IsEnabled = false);
+            Dispatcher.Invoke(() => progressBar.Value = 100);
+        });
+    }
+    public byte[] Encryp(ref string ivAsBase64, ref string keyAsBase64, ref string encryptedTextAsBase64, string text, string password)
+    {
+        var aes = Aes.Create();
+
+        aes.GenerateIV();
+        byte[] iv = aes.IV;
+        aes.KeySize = _keySize;
+        ivAsBase64 = Convert.ToBase64String(iv);
+
+        aes.GenerateKey();
+
+        string key = password;
+
+        byte[] keyBytes = Encoding.UTF8.GetBytes(key.Substring(0, aes.Key.Length));
+        aes.Key = keyBytes;
+
+        keyAsBase64 = Convert.ToBase64String(aes.Key);
+
+        byte[] textBytes = Encoding.UTF8.GetBytes(text);
+        var cryptor = aes.CreateEncryptor();
+        byte[] encryptedBytes = cryptor.TransformFinalBlock(textBytes, 0, textBytes.Length);
+        encryptedTextAsBase64 = Convert.ToBase64String(encryptedBytes);
+        return encryptedBytes;
+
     }
 
-    private void DecryptFile(string inputFilePath, string outputFilePath, string password, CancellationToken cancellationToken)
+
+    public byte[] Decryp(ref string ivAsBase64, ref string keyAsBase64, ref string encryptedTextAsBase64)
     {
-        using var inputStream = new FileStream(inputFilePath, FileMode.Open, FileAccess.Read);
-        using var outputStream = new FileStream(outputFilePath, FileMode.Create, FileAccess.Write);
-
         var aes = Aes.Create();
-        aes.Key = GetKey(password, aes);
-        aes.IV = GetRandomBytes(16);
+        aes.KeySize = _keySize;
+        byte[] iv = Convert.FromBase64String(ivAsBase64);
+        byte[] keyBytes = Convert.FromBase64String(keyAsBase64);
+        byte[] fromBase64ToBytes = Convert.FromBase64String(encryptedTextAsBase64);
 
-        var decryptor = aes.CreateDecryptor();
-
-        using var cryptoStream = new CryptoStream(inputStream, decryptor, CryptoStreamMode.Read);
-
-        var buffer = new byte[4096];
-        long totalBytes = inputStream.Length;
-        long processedBytes = 0;
-
-        while (true)
-        {
-            cancellationToken.ThrowIfCancellationRequested();
-
-            int bytesRead = cryptoStream.Read(buffer, 0, buffer.Length);
-            Thread.Sleep(1500);
-            if (bytesRead == 0) break;
-
-            outputStream.Write(buffer, 0, bytesRead);
-
-            processedBytes += bytesRead;
-            int progress = (int)Math.Round(processedBytes * 100.0 / totalBytes);
-            progressBar.Value = progress;
-        }
-        inputStream.Seek(0, SeekOrigin.Begin);
-        outputStream.Seek(0, SeekOrigin.Begin);
-        outputStream.Flush();
+        var decryptor = aes.CreateDecryptor(keyBytes, iv);
+        byte[] decryptedBytes = decryptor.TransformFinalBlock(fromBase64ToBytes, 0, fromBase64ToBytes.Length);
+        return decryptedBytes;
     }
 
-    private void CancelButton_Click(object sender, RoutedEventArgs e)
+
+
+    private void btnCancel_Click(object sender, RoutedEventArgs e)
     {
         _cts?.Cancel();
-    }
+        btnCancel.IsEnabled = false;
 
-
-
-    private byte[] GetKey(string password, SymmetricAlgorithm algorithm)
-    {
-        var salt = new byte[8];
-        using var rng = new RNGCryptoServiceProvider();
-        rng.GetBytes(salt);
-
-        var deriveBytes = new Rfc2898DeriveBytes(password, salt, 10000);
-        return deriveBytes.GetBytes(algorithm.KeySize / 8);
-    }
-
-    private byte[] GetRandomBytes(int size)
-    {
-        var bytes = new byte[size];
-        using var rng = new RNGCryptoServiceProvider();
-        rng.GetBytes(bytes);
-        return bytes;
     }
 
 }
